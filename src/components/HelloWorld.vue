b4d51c990e68b56ff4a31b301c9dac1e21f508da
Carlos Rosado <krlos_1594@yahoo.com>
2022-12-27 16:40:25 -0600

    w3a config

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,82 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <h2>Web3Auth X Vue.js</h2>
+    <section>
+      <button class="rpcBtn" @click="login" style="cursor: pointer">Login</button>
+      <!-- <button class="rpcBtn" @click="getUserInfo" style="cursor: pointer">Get User Info</button> -->
+      <!-- <button class="rpcBtn" @click="getChainId" style="cursor: pointer">Get Chain ID</button> -->
+      <!-- <button class="rpcBtn" @click="getAccounts" style="cursor: pointer">Get Accounts</button> -->
+      <!-- <button class="rpcBtn" @click="getBalance" style="cursor: pointer">Get Balance</button> -->
+      <!-- <button class="rpcBtn" @click="sendTransaction" style="cursor: pointer">Send Transaction</button> -->
+      <!-- <button class="rpcBtn" @click="signMessage" style="cursor: pointer">Sign Message</button> -->
+      <!-- <button class="rpcBtn" @click="getPrivateKey" style="cursor: pointer">Get Private Key</button> -->
+      <button class="rpcBtn" @click="logout" style="cursor: pointer">Logout</button>
+    </section>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+import { ref, onMounted } from "vue";
+import { Web3Auth } from "@web3auth/modal";
+import { CHAIN_NAMESPACES } from "@web3auth/base";
+
+export default {
+  name: "Ho-me",
+  props: {
+    msg: String,
+  },
+  setup() {
+    const loading = ref(false);
+    const loginButtonStatus = ref("");
+    const connecting = ref(false);
+    let provider = ref(null);
+
+    const clientId = "BHg_WXvNtmEGDBwGfJ5nPUf1J9JvKM_UJB_9sOCSK3wHNXfEEV4rsePnuDt0oTr1vSEBk22cq-tXoXlbHwmXUys"; // get from https://dashboard.web3auth.io
+
+      const web3auth = new Web3Auth({
+        clientId,
+        chainConfig: {
+          chainNamespace: CHAIN_NAMESPACES.EIP155,
+          chainId: "0x1",
+          rpcTarget: "https://rpc.ankr.com/eth", // This is the public RPC we have added, please pass on your own endpoint while creating an app
+        },
+      });
+
+    onMounted(async () => {
+      try {
+        loading.value = true;
+
+
+      await web3auth.initModal();
+        if (web3auth.provider) {
+          provider = web3auth.provider;
+        }
+      } catch (error) {
+        console.log("error", error);
+        console.log("error", error);
+      } finally {
+        loading.value = false;
+      }
+    });
+
+    const login = async () => {
+      if (!web3auth) {
+        console.log("web3auth not initialized yet");
+        return;
+      }
+      provider = await web3auth.connect();
+      console.log('provider: ', provider);
+    };
+
+    return {
+      loading,
+      loginButtonStatus,
+      connecting,
+      provider,
+      web3auth,
+      login,
+    };
+  },
+};
+</script>
